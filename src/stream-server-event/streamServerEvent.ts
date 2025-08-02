5d0f7681b2342596f8e9d3f0ff436c5bdc1a01f7
Alfian Andi Nugraha <me@alfianandi.dev>
2025-08-03 02:53:15 +0700

    fix(stream-server-event): improve reading stream by using TextDecoder for proper UTF-8 decoding

--- src/stream-server-event/streamServerEvent.ts
+++ src/stream-server-event/streamServerEvent.ts
@@ -5,14 +5,16 @@ const streamServerEvent = async (
   body: ReadableStream<Uint8Array>,
   options: StreamServerEventOptions = {},
 ) => {
-  const reader = body.pipeThrough(new TextDecoderStream()).getReader();
+  const reader = body.getReader();
+  const utf8Decoder = new TextDecoder('utf-8');
 
   return new Promise<void>(async (resolve) => {
     root: while (true) {
       const { done, value } = await reader.read();
 
       if (done) break;
-      const items = parseServerEvent(value);
+      const text = utf8Decoder.decode(value, { stream: true });
+      const items = parseServerEvent(text);
 
       for (const item of items) {
         if (item.type === 'message') {
